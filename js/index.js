1aeabfb474b3e556e036eb1486b8cfabb2b57c42
senthilmukeshhari <r.senthilkumar22bca106@gmail.com>
2024-10-23 18:42:40 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementsByClassName("navbar")[0].style.backgroundColor = "var(--primary-color)";
+  } else {
+    document.getElementsByClassName("navbar")[0].style.backgroundColor = "transparent";
+  }
+}
+
+const leftScroll = () => {
+  document.getElementById('cardWrapper').scrollBy({
+      left : -300,
+      behavior : 'smooth'
+  })
+}
+
+const rightScroll = () => {
+document.getElementById('cardWrapper').scrollBy({
+  left: 300,
+  behavior: 'smooth'
+});
+}
