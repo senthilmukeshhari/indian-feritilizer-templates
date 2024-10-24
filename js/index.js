7d38245d18a9c7c457a7526532434aded287f4d3
senthilmukeshhari <r.senthilkumar22bca106@gmail.com>
2024-10-24 22:34:52 +0530

    Update : Cards

--- js/index.js
+++ js/index.js
@@ -23,3 +23,21 @@ document.getElementById('cardWrapper').scrollBy({
   behavior: 'smooth'
 });
 }
+
+// fade animation
+document.addEventListener("DOMContentLoaded", function () {
+  var elements = document.querySelectorAll('.fade-in');
+
+  function checkVisibility() {
+    var windowHeight = window.innerHeight;
+    elements.forEach(function (element) {
+      var elementTop = element.getBoundingClientRect().top;
+      if (elementTop < windowHeight) {
+        element.classList.add('visible');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', checkVisibility);
+  checkVisibility();  // Check visibility on page load
+});
